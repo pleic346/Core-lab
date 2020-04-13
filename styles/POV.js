0747ce54a1c09da64b6834f22ecbf30b3262c3e1
Carmen <carmenpleitez20@gmail.com>
2020-04-13 18:32:33 -0400

    sss

---
+++ styles/POV.js
@@ -0,0 +1,83 @@
+var clickOne = document.querySelector("#class1");
+var dogOne = document.querySelector("#text1");
+
+clickOne.addEventListener("mouseover", function(){
+  dogOne.classList.toggle("appear")
+  })
+
+  var clickClass = document.querySelector("#class2");
+var dogText = document.querySelector("#text2");
+
+clickClass.addEventListener("mouseover", function(){
+  dogText.classList.toggle("appear")
+  })
+  
+  var clickCore = document.querySelector("#class3");
+  var dogTro = document.querySelector("#text3");
+  
+  clickCore.addEventListener("mouseover", function(){
+    dogTro.classList.toggle("appear")
+    })
+    
+    var clickFourth = document.querySelector("#class4");
+    var dogFourth = document.querySelector("#text4");
+    
+    clickFourth.addEventListener("mouseover", function(){
+      dogFourth.classList.toggle("appear")
+      })
+      
+      var clickFifth = document.querySelector("#class5");
+      var dogFifth = document.querySelector("#text5");
+      
+      clickFifth.addEventListener("mouseover", function(){
+        dogFifth.classList.toggle("appear")
+        })
+        
+        var clickSix = document.querySelector("#class6");
+        var dogSix = document.querySelector("#text6");
+        
+        clickSix.addEventListener("mouseover", function(){
+          dogSix.classList.toggle("appear")
+          })
+          
+          var clickSeven = document.querySelector("#class7");
+          var dogSeven = document.querySelector("#text7");
+          
+          clickSeven.addEventListener("mouseover", function(){
+            dogSeven.classList.toggle("appear")
+            })
+            
+            var clickEight = document.querySelector("#class8");
+            var dogEight = document.querySelector("#text8");
+            
+            clickEight.addEventListener("mouseover", function(){
+              dogEight.classList.toggle("appear")
+              })
+              
+              var clickNine = document.querySelector("#class9");
+              var dogNine = document.querySelector("#text9");
+              
+              clickNine.addEventListener("mouseover", function(){
+                dogNine.classList.toggle("appear")
+                })
+
+                var clickTen = document.querySelector("#class10");
+                var dogTen = document.querySelector("#text10");
+                
+                clickTen.addEventListener("mouseover", function(){
+                  dogTen.classList.toggle("appear")
+                  })
+                  var clickEleven = document.querySelector("#class11");
+                  var dogEleven = document.querySelector("#text11");
+                  
+                  clickEleven.addEventListener("mouseover", function(){
+                    dogEleven.classList.toggle("appear")
+                    })
+                  
+                    var clickTwelve = document.querySelector("#class12");
+                    var dogTwelve = document.querySelector("#text12");
+                    
+                    clickTwelve.addEventListener("mouseover", function(){
+                      dogTwelve.classList.toggle("appear")
+                      })
+                
