b61f1ac0c4b40996c6a86caa1ec77529c2e297d0
Carmen <carmenpleitez20@gmail.com>
2020-05-11 14:40:47 -0400

    m

---
+++ work.js
@@ -0,0 +1,10 @@
+
+
+
+var clickCircle = document.querySelector(".flex-image");
+var dogPic = document.querySelector(".flex-im");
+
+clickCircle.addEventListener("mouseover", function(){
+  dogPic.classList.toggle("appear")
+  
+})
